8b5d7110e8681ef19fe41335a4c62064afca11c6
wptheme4me <32770934+wptheme4me@users.noreply.github.com>
2020-12-04 12:54:52 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## uasociacija.eu
+
+Building site. Need content.
+
+## Configuration notes
+
+NPM package security check in DEV/PROD enviroments. Need to have nsp installed globally.
+
+   `npm i nsp -global`
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    site_title: "uasociacija.eu",
+    dev_siteurl: "http://localhost:3000/",
+    prod_siteurl: "https://uasociacija-eu.netlify.com/",
+    prod_siteurl_fb_img: "https://uasociacija-eu.netlify.com/img/logo.png",
+    google_analytics_key: "API-KEY",
+    facebook_appid: "APP_ID",
+    style_sheet: "/css/style.css",
+}
