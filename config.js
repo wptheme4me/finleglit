7d62b71d7e6a7e0b6cbd932e6a22a080fe00ef30
wptheme4me <hey@aperture.lt>
2021-10-12 02:11:24 +0300

    main URL in config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     site_title: "finleglit.eu",
     dev_siteurl: "http://localhost:3000/",
-    prod_siteurl: "https://finleglit-eu.netlify.app/",
-    prod_siteurl_fb_img: "https://finleglit-eu.netlify.app/img/logo.png",
+    prod_siteurl: "https://finleglit.eu/",
+    prod_siteurl_fb_img: "https://finleglit.eu/img/logo.png",
     google_analytics_key: "G-8L9PHJH7D7",
     facebook_appid: "APP_ID",
     style_sheet: "/css/style.css",
